a140641246c40187e5e3b1d95865ceba553491f7
Ragil Hadi Prasetyo <ragilprasetyo310@gmail.com>
2020-10-23 14:42:04 +0700

    Navigation next router

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
     return (
+        <>
         <div>
             Next.js App
         </div>
+        <Link href="/notes">
+                <a>
+                    Notes
+                </a>
+            </Link>
+        </>
     )
 }
 
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
-    return (
+    const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map(note => (
         <div>
-            Notes Next.js
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default index
